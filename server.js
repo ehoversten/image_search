0278fa7a5209cf7e199bd9e80f39cd1eda5600db
ehoversten <ehoversten@gmail.com>
2021-01-15 14:27:57 -0500

    loading temp data array to populate images to view

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 const pexels = require('pexels');
 const client = pexels.createClient(process.env.API_KEY);
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -21,15 +20,14 @@ app.use(express.static('assets'));
 app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
-// -- TEMP DATA SET -- //
-let dataSet = [];
+// -- TEMP DATA SET -- Database coming ... //
+const dataSet = [];
 
 // -- ROUTES -- //
 app.get('/', (req, res) => {
     console.log("Hit Landing Page");
     res.render("index", {});
 });
-
 
 app.get('/api', (req, res) => {
     // -- TESTING -- //
@@ -40,16 +38,19 @@ app.get('/api', (req, res) => {
 });
 
 app.post('/api', (req, res) => {
+    // Empty data array
+    dataSet.splice(0);
 
     // Store query string
     let query = req.body.search;
-    console.log(query);
     // Make API request
     client.photos.search({ query, per_page: 5 })
         .then(data => {
             let photos_data = data.photos;
-            console.log(photos_data);
-            dataSet = photos_data;
+            // console.log(photos_data);
+
+            // Load data set
+            photos_data.map(elem => dataSet.push(elem));
             res.redirect('/api');
         }).catch(err => {
             console.log(err);
