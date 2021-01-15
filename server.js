5d2824f90a27dfb7495a0a9b8237f46a7a1e724e
ehoversten <ehoversten@gmail.com>
2021-01-14 21:49:13 -0500

    setup template view engine

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const API = require("./utils");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// -- MIDDLEWARE --//
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('assets'));
+
+// Setup Template Engine
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+
+
+
 // -- ROUTES -- //
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     console.log("Hit Landing Page");
-    res.send("Welcome ...");
+    res.render("index", {});
+});
+
+
+app.get('/api', (req, res) => {
+
+    let data = 0;
+    res.json(data);
 })
 
 app.listen(PORT, () => {
